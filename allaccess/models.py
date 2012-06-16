78315a3062aa7bebc346e17484d7d49888c1ed30
mlavin <markdlavin@gmail.com>
2012-06-15 21:52:02 -0400

    Model for storing authorized user data.

--- allaccess/models.py
+++ allaccess/models.py
@@ -1,3 +1,19 @@
+from __future__ import unicode_literals
+from datetime import datetime
+
 from django.db import models
 
-# Create your models here.
+
+class AccountAccess(models.Model):
+    "Authorized remote service."
+
+    identifier = models.CharField(max_length=255)
+    service = models.CharField(max_length=50)
+    user = models.ForeignKey('auth.User', null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True, default=datetime.now)
+    modified = models.DateTimeField(auto_now=True, default=datetime.now)    
+    access_token = models.TextField(default='', blank=True)
+    raw_data = models.TextField(default='', blank=True)
+
+    class Meta(object):
+        unique_together = ('identifier', 'service')
