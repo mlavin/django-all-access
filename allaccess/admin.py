fb4add963346eb42ffecc6e32709c3bc522e309f
mlavin <markdlavin@gmail.com>
2012-06-22 20:08:04 -0400

    More customization to default Provider and AccountAccess admin.

--- allaccess/admin.py
+++ allaccess/admin.py
@@ -6,9 +6,14 @@ from .models import Provider, AccountAccess
 class ProviderAdmin(admin.ModelAdmin):
     "Admin customization for OAuth providers."
 
+    list_display = ('name', 'enabled', )
+
 
 class AccountAccessAdmin(admin.ModelAdmin):
     "Admin customization for accounts."
+
+    list_display = ('__unicode__', 'provider', 'user', 'created', 'modified', )
+    list_filter = ('provider', 'created', 'modified', )
 
 
 admin.site.register(Provider, ProviderAdmin)
