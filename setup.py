8431f5408e7c9ea037281eee84a7e9de35b1fcb4
mlavin <markdlavin@gmail.com>
2012-12-19 20:05:25 -0500

    Temporarily don't allow for requests 1.0 as a requirement.

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     long_description=read_file('README.rst'),
     install_requires=(
         'pycrypto>=2.3',
-        'requests>=0.13.0',
+        'requests>=0.13.0,<1.0',
         'oauthlib>=0.1.0,<0.2.0',
     ),
     tests_require=('mock>=0.8', ),
