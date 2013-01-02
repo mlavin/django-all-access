63162586ba2320a1e5b4e36923ec6cfeff863d4f
mlavin <markdlavin@gmail.com>
2013-01-01 23:21:05 -0500

    Adding very basic example application. See #16

---
+++ example/example/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+
+def home(request):
+    "Simple homepage view."
+    context = {}
+    if request.user.is_authenticated():
+        try:
+            access = request.user.accountaccess_set.all()[0]
+        except IndexError:
+            access = None
+        else:
+            client = access.api_client
+            context['info'] = client.get_profile_info(raw_token=access.access_token)
+    return render(request, 'home.html', context)
