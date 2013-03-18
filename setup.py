5f25cc8c68bc5dd649944acb7822377a8f0a9c5e
Mark Lavin <mlavin@caktusgroup.com>
2013-03-17 23:54:04 -0400

    Stray unicode_literal

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     include_package_data=True,
     url='https://github.com/mlavin/django-all-access',
     license='BSD',
-    description=u' '.join(__import__('allaccess').__doc__.splitlines()).strip(),
+    description=' '.join(__import__('allaccess').__doc__.splitlines()).strip(),
     classifiers=[
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Intended Audience :: Developers',
