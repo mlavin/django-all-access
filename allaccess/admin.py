3f0b19d153360ee5cf1fda1acfa0e4ad846b6c86
Antonin Lacombe <antonin@jelouemoncampingcar.com>
2015-08-27 09:54:39 +0200

    fix admin.py, remove site on AccountAccess and add it on Provider

--- allaccess/admin.py
+++ allaccess/admin.py
@@ -7,7 +7,8 @@ class ProviderAdmin(admin.ModelAdmin):
 
     "Admin customization for OAuth providers."
 
-    list_display = ('name', 'enabled', )
+    list_display = ('name', 'enabled', 'site',)
+    list_filter = ('name', 'enabled', 'site', )
 
 
 class AccountAccessAdmin(admin.ModelAdmin):
@@ -15,8 +16,8 @@ class AccountAccessAdmin(admin.ModelAdmin):
     "Admin customization for accounts."
 
     list_display = (
-        '__str__', 'provider', 'user', 'created', 'modified', 'site',)
-    list_filter = ('provider', 'created', 'modified', 'site', )
+        '__str__', 'provider', 'user', 'created', 'modified',)
+    list_filter = ('provider', 'created', 'modified', )
 
 
 admin.site.register(Provider, ProviderAdmin)
