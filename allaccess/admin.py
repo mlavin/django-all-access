615f1eb2376f7936d1966b809f9cff6337d7a416
Antonin Lacombe <antonin@jelouemoncampingcar.com>
2015-08-27 09:52:14 +0200

    add site on admin filter and display

--- allaccess/admin.py
+++ allaccess/admin.py
@@ -4,16 +4,19 @@ from .models import Provider, AccountAccess
 
 
 class ProviderAdmin(admin.ModelAdmin):
+
     "Admin customization for OAuth providers."
 
     list_display = ('name', 'enabled', )
 
 
 class AccountAccessAdmin(admin.ModelAdmin):
+
     "Admin customization for accounts."
 
-    list_display = ('__str__', 'provider', 'user', 'created', 'modified', )
-    list_filter = ('provider', 'created', 'modified', )
+    list_display = (
+        '__str__', 'provider', 'user', 'created', 'modified', 'site',)
+    list_filter = ('provider', 'created', 'modified', 'site', )
 
 
 admin.site.register(Provider, ProviderAdmin)
