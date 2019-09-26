f41b6283b042aff5f98f2b47d78c599ce96b06e6
Florian Demmer <fdemmer@gmail.com>
2019-09-26 11:03:18 +0200

    Fix optional params kwarg in OAuth2Client

--- allaccess/clients.py
+++ allaccess/clients.py
@@ -219,7 +219,7 @@ class OAuth2Client(BaseOAuthClient):
         user_token = kwargs.pop('token', self.token)
         token, _ = self.parse_raw_token(user_token)
         if token is not None:
-            params = kwargs.get('params', {})
+            params = kwargs.get('params') or {}
             params['access_token'] = token
             kwargs['params'] = params
         return super(OAuth2Client, self).request(method, url, **kwargs)
