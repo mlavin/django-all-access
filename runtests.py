caeddc60eeddf80fee027ba214b506deda0fa883
Julio Rama <julio.rama@corp.globo.com>
2020-02-18 17:51:03 -0300

    Updated Django version to 3.0.3 and fixed runtests.py

--- runtests.py
+++ runtests.py
@@ -26,13 +26,28 @@ if not settings.configured:
             }
         },
         INSTALLED_APPS=INSTALLED_APPS,
-        MIDDLEWARE_CLASSES=(
+        MIDDLEWARE=(
             'django.middleware.common.CommonMiddleware',
             'django.contrib.sessions.middleware.SessionMiddleware',
             'django.middleware.csrf.CsrfViewMiddleware',
             'django.contrib.auth.middleware.AuthenticationMiddleware',
             'django.contrib.messages.middleware.MessageMiddleware',
         ),
+        TEMPLATES = [
+            {
+                'BACKEND': 'django.template.backends.django.DjangoTemplates',
+                'DIRS': [],
+                'APP_DIRS': True,
+                'OPTIONS': {
+                    'context_processors': [
+                        'django.template.context_processors.debug',
+                        'django.template.context_processors.request',
+                        'django.contrib.auth.context_processors.auth',
+                        'django.contrib.messages.context_processors.messages',
+                    ],
+                },
+            },
+        ],
         AUTHENTICATION_BACKENDS=(
             'allaccess.backends.AuthorizedServiceBackend',
         ),
