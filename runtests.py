3ec2a9b3f53e75b3e0e55f37d331832d6d2252e1
Florian Demmer <fdemmer@gmail.com>
2021-05-19 17:30:05 +0200

    Add context_processor for Django 3.1 compatibility
    https://docs.djangoproject.com/en/3.2/releases/3.1/#django-contrib-admin

--- runtests.py
+++ runtests.py
@@ -39,6 +39,7 @@ if not settings.configured:
                 'context_processors': [
                     'django.contrib.auth.context_processors.auth',
                     'django.contrib.messages.context_processors.messages',
+                    'django.template.context_processors.request',
                 ]
             }
         }],
