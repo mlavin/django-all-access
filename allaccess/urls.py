115d38c135e99ee580f78f265ceb42e9fb201ed9
Florian Demmer <fdemmer@gmail.com>
2021-05-19 17:22:04 +0200

    Fix oversight when migrating to django.urls.path() in 9c32477c7c93b91b0f6349e4bc8afbd81e7768f3

--- allaccess/urls.py
+++ allaccess/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import OAuthCallback, OAuthRedirect
 
 urlpatterns = [
-    path('login/(?P<str:provider>/', OAuthRedirect.as_view(), name='allaccess-login'),
-    path('callback/(?P<str:provider>/', OAuthCallback.as_view(), name='allaccess-callback'),
+    path('login/<str:provider>/', OAuthRedirect.as_view(), name='allaccess-login'),
+    path('callback/<str:provider>/', OAuthCallback.as_view(), name='allaccess-callback'),
 ]
